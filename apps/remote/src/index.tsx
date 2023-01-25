f087e70835cd28b65773aa57750468d6f6f2b15a
Raman Nikitsenka <rnikitsenka@elinext.com>
2023-01-25 11:20:19 +0100

    chore: add bootstrap for remote

--- apps/remote/src/index.tsx
+++ apps/remote/src/index.tsx
@@ -1,13 +1,4 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
+import("./bootstrap");
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// eslint-disable-next-line import/no-anonymous-default-export
+export default true;
